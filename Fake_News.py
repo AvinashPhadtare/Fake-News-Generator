fdf6b2f1546ea23ee421a141ed9a78e2996bddfa
Avinash Phadtare <phadtareavinash2008@gmail.com>
2026-03-23 22:44:26 +0530

    Add files via upload

--- Fake_News.py
+++ Fake_News.py
@@ -1,65 +1,62 @@
-# # This is Fake News Generator, Who gives you fake news or also this is Funny
-# import random
+# This is Fake News Generator, Who gives you fake news or also this is Funny
+import random
 
-# subjects = ["Virat kohli","Rohit sharma","Shubhman gill","Sharukh khan","Deepika","Ranveer","Modi ji","Monkey form Mumbai"]
+subjects = ["Virat kohli","Rohit sharma","Shubhman gill","Sharukh khan","Deepika","Ranveer","Modi ji","Monkey form Mumbai"]
 
-# action = ["launches","eats","ridding","swimming","singing","dancing ","laughing "]
+action = ["launches","eats","ridding","swimming","singing","dancing ","laughing "]
 
-# places = ["in Mumbai's local train","at Red Fort","in Bathroom","at Public Place","at Gate way of India","at Dubai"]
+places = ["in Mumbai's local train","at Red Fort","in Bathroom","at Public Place","at Gate way of India","at Dubai"]
 
-# print("\n1. Do you want to Genetrate News\n2.Do you want to Add any Word\n")
-# choice = int(input("Enter a choice: "))
-# if choice == int(1):
-#     while True:
-#         sub = random.choice(subjects)
-#         act = random.choice(action)
-#         place = random.choice(places)
+print("\n1. Do you want to Genetrate News\n2.Do you want to Add any Word\n")
+choice = int(input("Enter a choice: "))
+if choice == int(1):
+    while True:
+        sub = random.choice(subjects)
+        act = random.choice(action)
+        place = random.choice(places)
 
-#         headline = f"BREAKING NEWS: {sub} {act} {place}\n"
-#         print(headline)
+        headline = f"BREAKING NEWS: {sub} {act} {place}\n"
+        print(headline)
 
-#         user_input = input("\nDo you want another One(yes/no): \n".lower().strip())
-#         if user_input == "no":
-#             break
+        user_input = input("\nDo you want another One(yes/no): \n".lower().strip())
+        if user_input == "no":
+            break
 
 
-# elif choice == int(2):
-#     print("What do you want to Change:-\n1. Subject\n2. Action\n3. Places")
-#     choice1 = int(input("Enter your choice:"))
-#     if choice1 == int(1):
-#         add_subject = input("Enter a Subject: ".capitalize())
-#         subjects.append(add_subject)
+elif choice == int(2):
+    print("What do you want to Change:-\n1. Subject\n2. Action\n3. Places")
+    choice1 = int(input("Enter your choice:"))
+    if choice1 == int(1):
+        add_subject = input("Enter a Subject: ".capitalize())
+        subjects.append(add_subject)
         
-#     elif choice1 == int(2):
-#         add_action = input("Enter a Action: ".lower())
-#         action.append(add_action)
+    elif choice1 == int(2):
+        add_action = input("Enter a Action: ".lower())
+        action.append(add_action)
         
-#     elif choice1 == int(3):
-#         add_place = input("Enter a Place: ")
-#         places.append(add_place)
+    elif choice1 == int(3):
+        add_place = input("Enter a Place: ")
+        places.append(add_place)
         
-#     else:
-#         print("You Entered Wrong Choice !..")
+    else:
+        print("You Entered Wrong Choice !..")
 
 
-#     while True:
-#         sub = random.choice(subjects)
-#         act = random.choice(action)
-#         place = random.choice(places)
+    while True:
+        sub = random.choice(subjects)
+        act = random.choice(action)
+        place = random.choice(places)
 
-#         headline = f"BREAKING NEWS: {sub} {act} {place}\n"
-#         print(headline)
+        headline = f"BREAKING NEWS: {sub} {act} {place}\n"
+        print(headline)
 
-#         user_input = input("\nDo you want another One(yes/no): \n".lower().strip())
-#         if user_input == "no":
-#             break
-
-
-# else:
-#     print("You Entred Wrong Choice!...")
+        user_input = input("\nDo you want another One(yes/no): \n".lower().strip())
+        if user_input == "no":
+            break
 
 
-# print("\nThanks for using Fake News Generator!.. GoodBye :)")
+else:
+    print("You Entred Wrong Choice!...")
 
-message = 'Avinash'
-print(message.__subclasshook__(message))
+
+print("\nThanks for using Fake News Generator!.. GoodBye :)")
